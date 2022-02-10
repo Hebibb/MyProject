1524c2f613fe061f8a13855c0b168860b136045a
Hebibb <bhabibb1995@gmail.com>
2022-02-11 00:37:14 +0400

    database deployment

---
+++ app/routes.py
@@ -0,0 +1,38 @@
+from run import app
+from flask import render_template
+
+
+@app.route("/",methods=['GET','POST'])
+def app_index():
+   return render_template('app/index.html')
+
+
+
+users=[]
+@app.route("/admin/contact",methods=['GET','POST'])
+def contact():
+    if request.method=='POST':
+        _ad=request.form['ad ']
+        _soyad=request.form['soyad ']
+        _telefon=request.form['telefon ']
+        _email=request.form['email ']
+        _message=request.form['message ']
+        _date=datetime.date.today()
+        user={
+            'ad':_ad,
+            'soyad':_soyad,
+            'telefon':_telefon,
+            'email':_email,
+            'message':_message,
+            'date':_date
+        }
+        users.append(user)
+        # return redirect('/')
+
+    return render_template('admin/contact.html',istifadeciler=users)
+
+
+
+
+
+
---
+++ run.py
@@ -0,0 +1,24 @@
+from flask import Flask,redirect,url_for,render_template,request
+from flask_sqlalchemy import SQLAlchemy
+app=Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///contact.db'
+db = SQLAlchemy(app)
+class Contact(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    ad_soyad=db.Column(db.String(40))
+    telefon=db.Column(db.String(10))
+    email=db.Column(db.String(20))
+    message=db.Column(db.String(200))
+    ad_soyad=db.Column(db.String(50))
+    
+db.create_all()
+
+
+from admin.routes import *
+from app.routes import *
+
+
+
+if __name__ == '__main__':
+ 
+    app.run(port=5000,debug=True)
